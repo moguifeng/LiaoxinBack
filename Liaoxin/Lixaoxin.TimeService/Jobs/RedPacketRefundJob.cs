6cd5490d4f36ac9654f7f2da88ce63427f3543ee
Guowei_He <Guowei_He@Jabil.com>
2021-05-13 10:57:03 +0800

    增加定时作业框架

---
+++ Liaoxin/Lixaoxin.TimeService/Jobs/RedPacketRefundJob.cs
@@ -0,0 +1,27 @@
+﻿using Lixaoxin.TimeService.Utility;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lixaoxin.TimeService.Jobs
+{
+    /// <summary>
+    /// 红包退款:特性已控制不并发执行
+    /// </summary>
+    [DisallowConcurrentExecution]
+    public class RedPacketRefundJob : IJob
+    {
+  
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            DateTime yesterdayTime = DateTime.Now.AddDays(-1);
+            DataTable dt = MySqlHelper.ExecuteDataTable($"SELECT RedPacketId,ClientId,Over FROM redpackets WHERE STATUS=0 AND Over>0 AND IsEnable=TRUE AND SendTime<='{yesterdayTime.ToString("yyyy-MM-dd HH:mm:ss")}'");
+
+  
+        }
+    }
+}
---
+++ Liaoxin/Lixaoxin.TimeService/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Hosting;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Topshelf;
+
+namespace Lixaoxin.TimeService
+{
+    class Program
+    {
+        static ServiceRunner QuartzServiceRunner = null;
+
+        static void Main(string[] args)
+        {
+            var rc = HostFactory.Run(x =>
+            {
+                
+                x.Service<ServiceRunner>(s =>
+                {
+                    s.ConstructUsing(name => new ServiceRunner());
+                    s.WhenStarted((tc, hc) => tc.Start(hc));
+                    s.WhenStopped((tc, hc) => tc.Stop(hc));
+                    s.WhenContinued((tc, hc) => tc.Continue(hc));
+                    s.WhenPaused((tc, hc) => tc.Pause(hc));
+                    
+                });
+
+                x.RunAsLocalService();
+                x.StartAutomaticallyDelayed();
+                x.RunAsLocalSystem();
+
+                x.SetDescription("测试后台服务");
+                x.SetDisplayName("TestServer");
+                x.SetServiceName("TestServer");
+                x.EnablePauseAndContinue();
+            });
+          
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+            Environment.ExitCode = exitCode;
+        }
+
+
+
+
+    }
+}
